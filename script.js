7b550206ea58badef14ec82df26b2a992f1cf903
JuanXCV <jaconesab@agrodolores.es>
2019-09-30 17:53:12 +0200

    responsive

--- script.js
+++ script.js
@@ -11,8 +11,28 @@ $(document).ready( function(){
     $('.hamburger').toggleClass('open');
     $('#nav').toggleClass('hidden')
     $('#nav').removeClass('none')
+    
+  });
 
-});
+  $('.img-vinted').click(function () {
+    $('#vin, #vin2, #vin3').toggleClass('vacio')
+  })
 
+  $('.img-vinted').hover(function () {
+      $('.img-vinted').css('transform','scale(1.25)');
+    }, function() {
+      $('.img-vinted').css('transform','scale(1)');
+    }
+  );
 
+  $('.hover').hover(function () {
+      $(this).css('transform','scale(1.25)');
+    }, function() {
+      $(this).css('transform','scale(1)');
+    }
+  );
+
+  // $(document).scroll(function() {
+  //   alert('scroll')
+  // })
 })
